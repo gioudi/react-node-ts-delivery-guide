82c23f713211839daa602146c012f9febd499754
Sergio Penagos <sergiopenagos881@gmail.com>
2024-11-05 02:17:04 -0500

    feature/setup-jest

---
+++ design-system/src/components/DpsButton/types.ts
@@ -0,0 +1,8 @@
+export interface CustomButtonProps {
+    text: string;
+    onClick: () => void;
+    variant?: "text" | "contained" | "outlined";
+    loading?: boolean;
+    icon?: React.ReactNode;
+    disabled?: boolean;
+  }
